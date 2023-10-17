1c2fd5ad50be0ceb02102d8d2fc9efbd9c3cd9da
Костя <@https://github.com/skostia91/git-test>
2023-10-17 14:36:55 +0300

    3.0 добавил миграцию.Всё работает

---
+++ src/main/resources/db/migration/V3__insert_currencies.sql
@@ -0,0 +1,3 @@
+insert into currencies (code, fullname, sign) values ('EUR', 'Euro', '€');
+insert into currencies (code, fullname, sign) values ('USD', 'United States dollar', '$');
+insert into currencies (code, fullname, sign) values ('RUB', 'Russian Ruble', '₽');
---
+++ src/main/resources/db/migration/V4__insert_data_exchange_rates.sql
@@ -0,0 +1,3 @@
+insert into exchange_rates (base_currency_id, target_currency_id, rate) values (1, 2, 1.059);
+insert into exchange_rates (base_currency_id, target_currency_id, rate) values (1, 3, 103.75);
+insert into exchange_rates (base_currency_id, target_currency_id, rate) values (2, 3, 98);
